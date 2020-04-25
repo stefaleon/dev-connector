1e467f2673178dde252c40a745407dfe360a4249
stefaleon <stefaleon@gmail.com>
2020-04-25 17:48:50 +0300

    07 Connecting To MongoDB With Mongoose

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dev Connector 2.0
+
+## a MERN project
+
+## from [MERN Stack Front To Back: Full Stack React, Redux & Node.js](https://www.udemy.com/course/mern-stack-front-to-back)
+
+## by [Brad Traversy](https://www.traversymedia.com/)
---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('\x1b[36m%s\x1b[0m', 'Connected to MongoDB');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
