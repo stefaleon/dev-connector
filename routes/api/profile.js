ecbeab31d7e77cd7d36ae2d689d7f903e1c700ad
stefaleon <stefaleon@gmail.com>
2020-05-10 10:51:18 +0300

    21 Delete Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -213,4 +213,41 @@ router.put(
   }
 );
 
+//     // Get remove index
+//     const removeIndex = profile.experience
+//       .map(item => item.id)
+//       .indexOf(req.params.exp_id);
+
+//     profile.experience.splice(removeIndex, 1);
+
+//     await profile.save();
+
+//     res.json(profile);
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server Error');
+//   }
+// });
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    //const foundProfile = await Profile.findOneAndUpdate( { user: req.user.id },
+    //  { $pull: { experience: { _id: req.params.exp_id }}},
+    //  {new: true});
+    const foundProfile = await Profile.findOne({ user: req.user.id });
+
+    // Filter exprience array using _id (NOTE: _id is a BSON type needs to be converted to string)
+    // This can also be omitted and the next line and findOneAndUpdate to be used instead (above implementation)
+    foundProfile.experience = foundProfile.experience.filter(
+      (exp) => exp._id.toString() !== req.params.exp_id
+    );
+
+    await foundProfile.save();
+    return res.status(200).json(foundProfile);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: 'Server error' });
+  }
+});
+
 module.exports = router;
